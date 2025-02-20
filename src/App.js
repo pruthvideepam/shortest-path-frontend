cc1bced110689081a5d2092953d46c554f4d875b
pruthvideepam <157585780+pruthvideepam@users.noreply.github.com>
2025-02-20 12:52:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -177,12 +177,13 @@ const App = () => {
           />
         ))}
 
-        {routes.length > 0 && (
-          <>
-            <Marker position={[routes[selectedRouteIndex][0].lat, routes[selectedRouteIndex][0].lon]} />
-            <Marker position={[routes[selectedRouteIndex][routes[selectedRouteIndex].length - 1].lat, routes[selectedRouteIndex][routes[selectedRouteIndex].length - 1].lon]} />
-          </>
-        )}
+        {routes.length > 0 && routes[selectedRouteIndex] && routes[selectedRouteIndex].length > 0 && (
+  <>
+    <Marker position={[routes[selectedRouteIndex][0].lat, routes[selectedRouteIndex][0].lon]} />
+    <Marker position={[routes[selectedRouteIndex][routes[selectedRouteIndex].length - 1].lat, routes[selectedRouteIndex][routes[selectedRouteIndex].length - 1].lon]} />
+  </>
+)}
+
       </MapContainer>
     </div>
   );
